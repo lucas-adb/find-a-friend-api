bf84c81e581f301526c3dee8af94814481b2c5aa
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-23 18:14:16 -0300

    feat: creates simplified logic to create an org

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+import { prisma } from "./lib/prisma.js";
+import { hash } from "bcryptjs";
 
 export const app = fastify();
+
+app.post('/orgs', async (request: FastifyRequest, reply: FastifyReply) => {
+  const bodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    address: z.string(),
+    phone: z.string(),
+    password: z.string()
+  });
+
+  const { name, email, address, phone, password  } = bodySchema.parse(request.body);
+
+  const password_hash = await hash(password, 6);
+
+  await prisma.org.create({
+    data: {
+      name, email, address, phone, password_hash
+    }
+  });
+
+  reply.status(201).send();
+});
