f9e472d662c1e2f7c09ae3c1f20eab2b5da54343
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-24 19:50:24 -0300

    feat: create e2e test environments

---
+++ src/http/controllers/orgs/auth-org.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from 'vitest';
+
+describe('Auth Org e2e', () => {
+  test('should be able to register', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,24 @@ import tsconfigPaths from 'vite-tsconfig-paths'; // to recognize aliases
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    // todo
+    dir: 'src',
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+    ],
   },
 });
