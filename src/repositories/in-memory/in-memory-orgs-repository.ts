63437f72256568f0ece6e4aae2053e47971a15c8
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-24 17:06:58 -0300

    test: creat org use case (unit tests)

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,31 @@
+import { Prisma, Org } from '@prisma/client';
+import { OrgsRepository } from '../orgs-repository.js';
+import { randomUUID } from 'node:crypto';
+
+export class InMemoryOrgRepository implements OrgsRepository {
+  public items: Org[] = [];
+
+  async create(data: Prisma.OrgCreateInput): Promise<Org> {
+    const org = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      address: data.address,
+      phone: data.phone,
+      password_hash: data.password_hash,
+    };
+
+    this.items.push(org);
+
+    return org;
+  }
+  async findByEmail(email: string): Promise<Org | null> {
+    const org = this.items.find((item) => item.email === email);
+
+    if (!org) {
+      return null;
+    }
+
+    return org;
+  }
+}
---
+++ src/use-cases/create-org.spec.ts
@@ -0,0 +1,61 @@
+import { InMemoryOrgRepository } from '@/repositories/in-memory/in-memory-orgs-repository.js';
+import { beforeEach, describe, expect, test } from 'vitest';
+import { CreateOrgUseCase } from './create-org.js';
+import { compare } from 'bcryptjs';
+import { OrgAlreadyExistsError } from './errors/org-already-exists-error.js';
+
+let repository: InMemoryOrgRepository;
+let sut: CreateOrgUseCase;
+
+describe('Create Org', () => {
+  beforeEach(() => {
+    repository = new InMemoryOrgRepository();
+    sut = new CreateOrgUseCase(repository);
+  });
+
+  test('creates org', async () => {
+    const data = {
+      name: 'Lucas Alves',
+      email: 'lucas@mail.com',
+      address: 'rua x',
+      phone: '99999999',
+      password: '123456',
+    };
+
+    const { org } = await sut.create(data);
+
+    expect(org.id).toEqual(expect.any(String));
+  });
+
+  test('hashes password', async () => {
+    const data = {
+      name: 'Lucas Alves',
+      email: 'lucas@mail.com',
+      address: 'rua x',
+      phone: '99999999',
+      password: '123456',
+    };
+
+    const { org } = await sut.create(data);
+
+    const isPasswordHashed = await compare('123456', org.password_hash);
+
+    expect(isPasswordHashed).toBe(true);
+  });
+
+  test('cant create two orgs with same email', async () => {
+    const data = {
+      name: 'Lucas Alves',
+      email: 'lucas@mail.com',
+      address: 'rua x',
+      phone: '99999999',
+      password: '123456',
+    };
+
+    await sut.create(data);
+
+    expect(async () => {
+      await sut.create(data);
+    }).rejects.toBeInstanceOf(OrgAlreadyExistsError);
+  });
+});
